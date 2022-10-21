5b19596b4b19671498166d5241698e6e37908656
Yaser Altaiy <yaseraltaiy@gmail.com>
2022-10-21 04:58:14 +0300

    t3dilat

--- auctions/static/carwash.js
+++ auctions/static/carwash.js
@@ -1,6 +1,21 @@
 var z = document.getElementById("demo");
 var x;
 var y;
+var map;
+var markers = [
+    {
+        position: {lat: 33.398165, lng: 44.3960218},
+        title:'first'
+    },
+    {
+        position: {lat: 33.3676544, lng: 44.3580416},
+        title: 'second'
+    },
+    {
+        position: {lat: 33.3364, lng: 44.4004},
+        title: 'third'
+    }
+];
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -11,19 +26,27 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    z.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-
+    z.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
     x = position.coords.latitude;
     y = position.coords.longitude;
-
     var currentlocation = { lat: x, lng: y };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
         center: currentlocation
     });
-    var marker = new google.maps.Marker({
-        position: currentlocation,
-        map: map
-    });
+    // var image = 'static/carwash.png'
+    // var marker = new google.maps.Marker({
+    //     position: currentlocation,
+    //     map: map,
+    //     icon: image,
+    //     tital: "my location"
+    // });
+
+    // markers.forEach( function(loc) {
+    //     var mark = new google.maps.Marker({
+    //         position: loc.position,
+    //         title: loc.title,
+    //         map: map,
+    //     })
+    // });
 }
